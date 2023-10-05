2a93fb9d9ea1681d0d9d184a83ccc81e7fea4ae1
MegaProMatvey <146074631+MegaProMatvey@users.noreply.github.com>
2023-10-05 17:47:32 +0800

    Add files via upload

---
+++ test/test1/task2.py
@@ -0,0 +1,3 @@
+num1, num2 = int(input()), int(input())
+print('Квадрат суммы', num1, 'и', num2, 'равен', (num1 + num2) ** 2)
+print('Сумма квадратов', num1, 'и', num2, 'равна', num1 ** 2 + num2 ** 2)
